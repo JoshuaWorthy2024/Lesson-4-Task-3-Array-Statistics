3119f7fb65c089582278941e7364b45b8be50b14
JoshuaWorthy2024 <joshuaworthy@cgspectrum.institute>
2025-02-02 13:33:12 -0600

    Attempt 1 (with modified 20 values)

--- Lesson 4 Task 3 Array Statistics/Main.cpp
+++ Lesson 4 Task 3 Array Statistics/Main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
+#include <array>
 using std::endl;
 using std::cout;
 using std::cin;
-
 /*
 Practice Task 3: Array Statistics
 Objective: Work with arrays to store data and calculate statistics.
@@ -23,6 +23,115 @@ Bonus Task:
 Modify the program to handle an array of 20 values.
 Reuse the same functions to calculate the statistics for this larger array.
 */
-int main() {
 
+int sumOfArray(int array[], int length) {
+	int sum = 0;
+	for (int i = 0; i < length; i++) {
+		sum += array[i];
+	}
+	return sum;
+}
+
+int averageOfArray(int array[], int length) {
+	int sum = 0;
+	for (int i = 0; i < length; i++) {
+		sum += array[i];
+	}
+	return sum / length;
+}
+
+int getSmallestNumber(int array[], int length) {
+	int number = array[0];
+	for (int i = 0; i < length; i++) {
+		if (array[i] == number)
+			continue;
+
+		if (array[i] < number) {
+			number = array[i];
+		}
+	}
+	return number;
+}
+
+int getLargestNumber(int array[], int length) {
+	int number = array[0];
+	for (int i = 0; i < length; i++) {
+		if (array[i] == number)
+			continue;
+
+		if (array[i] > number) {
+			number = array[i];
+		}
+	}
+	return number;
+}
+
+int getOddOrEvenNumbers(int array[], int length, bool lookingForOdd) {
+	int counter = 0;
+	for (int i = 0; i < length; i++) {
+		if (array[i] % 2 == 0) {
+			if (!lookingForOdd) {
+				counter++;
+			}
+		}
+		else {
+			if (lookingForOdd) {
+				counter++;
+			}
+		}
+	}
+
+	return counter;
+}
+
+int main() {
+	const int arrayLength = 20;
+	int arrayOfNumbers[arrayLength];
+
+	for (int i = 0; i < arrayLength; i++) {
+		arrayOfNumbers[i] = 0;
+	}
+
+	int counter = 0;
+	int enteredNumber = 0;
+	
+	cout << "Please enter " << arrayLength << " numeric values." << endl;
+	
+	for (; counter < arrayLength; counter++) {
+		cout << (counter + 1) << ") ";
+		cin >> enteredNumber;
+		// -- cin check
+		arrayOfNumbers[counter] = enteredNumber;
+	}
+
+	counter = 0;
+	for (; counter < arrayLength; counter++) {
+		cout << arrayOfNumbers[counter];
+		if (counter == arrayLength - 1) {
+			cout << "." << endl << endl;
+		}
+		else {
+			cout << + ", ";
+		}
+	}
+
+	int sum = sumOfArray(arrayOfNumbers, arrayLength);
+	cout << "Sum: " << sum << endl;
+
+	int average = averageOfArray(arrayOfNumbers, arrayLength);
+	cout << "Average: " << average << endl;
+
+	int smallestNumber = getSmallestNumber(arrayOfNumbers, arrayLength);
+	int largestNumber = getLargestNumber(arrayOfNumbers, arrayLength);
+
+	cout << "Smallest Number: " << smallestNumber << endl;
+	cout << "Largest Number: " << largestNumber << endl;
+
+	int amountOfOddNumbers = getOddOrEvenNumbers(arrayOfNumbers, arrayLength, true);
+	int amountOfEvenNumbers = getOddOrEvenNumbers(arrayOfNumbers, arrayLength, false);
+
+	cout << "Amount of Odd Numbers: " << amountOfOddNumbers << endl;
+	cout << "Amount of Even Numbers: " << amountOfEvenNumbers << endl;
+
+	return 0;
 }
