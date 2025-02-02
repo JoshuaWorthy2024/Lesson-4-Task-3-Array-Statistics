f6aaa8bf3b264f9280433192f44ef62db5ad1518
JoshuaWorthy2024 <joshuaworthy@cgspectrum.institute>
2025-02-02 04:22:15 -0600

    Add project files.

---
+++ Lesson 4 Task 3 Array Statistics/Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+using std::cin;
+
+/*
+Practice Task 3: Array Statistics
+Objective: Work with arrays to store data and calculate statistics.
+
+Instructions:
+
+Write a program that:
+Asks the user to input 10 integer values.
+Stores these values in an array.
+Uses a loop to print the array contents.
+Calculates and prints the following statistics:
+The sum of all values.
+The average of the values.
+The largest and smallest values.
+The number of even and odd values.
+Bonus Task:
+
+Modify the program to handle an array of 20 values.
+Reuse the same functions to calculate the statistics for this larger array.
+*/
+int main() {
+
+}
